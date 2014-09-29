5285b5e81dde19271d965741cc891e06e884536d
phtaylor <phil.taylor@fabricengine.com>
2014-09-29 13:29:16 -0400

    fixed build break.

--- DGGraphImpl.cpp
+++ DGGraphImpl.cpp
@@ -553,7 +553,7 @@ bool DGGraphImpl::loadExtension(const std::string & extName, std::string * error
       FabricCore::KLSourceFile klSourceFile[1];
       klSourceFile[0].filenameCStr = fileName.c_str();
       klSourceFile[0].sourceCodeCStr = klCode.c_str();
-      RegisterKLExtension(*sClient, name.c_str(), "", 1, klSourceFile, false);
+      RegisterKLExtension(*sClient, name.c_str(), "", 1, klSourceFile, false, false);
     }
     catch(FabricCore::Exception e)
     {
