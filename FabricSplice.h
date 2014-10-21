51bcea82bfe943ca18c7472d28609596c3a93825
Helge Mathee <helge.mathee@fabricengine.com>
2014-10-21 12:25:56 +0200

    #3406: fix explicit_bool.h

--- FabricSplice.h
+++ FabricSplice.h
@@ -1604,7 +1604,6 @@ Class Outline
 
 #include <stdint.h>
 #include <stdio.h>
-#include "explicit_bool.h"
 
 #if defined(_MSC_VER) || defined(SWIGWIN)
 # if defined(__cplusplus)
@@ -1657,6 +1656,41 @@ Class Outline
 
 // C typedefs
 //=====================================================
+
+//
+// This template class allows for defining explicit boolean
+// version operators without C++0x
+//
+// This code is taken (mostly) verbatim from 
+// http://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Safe_bool
+// modified so the actual conversion operators is no hidden on the base class 
+//
+// Usage is like so
+//
+//
+//  class YourClass : 
+//    public safe_bool <YourClass> // CRTP idiom
+//  {
+//  public:
+//    // Define boolean conversion
+//    operator explicit_bool::type() const {
+//      return explicit_bool::get(SomeLogicHere()); 
+//    }
+//  };
+//
+//   This will allow
+// if (yourClass)
+//   but not
+// int var = yourClass;
+class explicit_bool {
+  public:
+    typedef void (explicit_bool::*type)() const;
+    void this_type_does_not_support_comparisons() const {}
+  
+    static type get(bool condition) {
+        return condition ? &explicit_bool::this_type_does_not_support_comparisons : 0;
+    }
+};
 
 struct FECS_PersistenceInfo
 {
