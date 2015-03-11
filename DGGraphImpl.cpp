d6a50489dc8be3d4d43b4f8bb8f9c075a76ebf50
Helge Mathee <helge.mathee@fabricengine.com>
2015-03-11 08:54:41 +0100

    FE-3880 add support for DGNode.setDirty

--- DGGraphImpl.cpp
+++ DGGraphImpl.cpp
@@ -3090,6 +3090,10 @@ bool DGGraphImpl::requireEvaluate()
 {
   if(mRequiresEval)
     return false;
+
+  for(DGNodeIt nodeIt = mDGNodes.begin(); nodeIt != mDGNodes.end(); nodeIt++)
+    nodeIt->second.node.setDirty();
+
   mRequiresEval = true;
   return true;
 }
